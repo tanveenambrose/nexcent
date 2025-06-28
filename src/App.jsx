39ee55a5952393317ea7d0c2335e50dc824024d1
TanveenAmbrose <racoctanveen15@gmail.com>
2025-06-28 10:54:44 +0600

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './index.css';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 function App() {
   return (
     <>
     <Navbar/>
+    <Home/>
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Logo from '../assets/Icon.svg'
 import {Link} from 'react-scroll'
+import { FaBars, FaXmark } from 'react-icons/fa6'
 
 const Navbar = () => {
     const [isMenuOpen,setMenuOpen]= useState(false);
@@ -37,8 +38,9 @@ const Navbar = () => {
 
   return (
     <header className='w-full bg-white md:bg-transparent fixed top-0 left-0 right-0'>
-        <nav>
-            <a href="" className='text-2xl font-semibold flex items-center space-x-3'><img src={Logo} alt="" className='w-10 inline-block items-center' /><span className='text-[#263238]'>Nexcent</span></a>
+        <nav className={`py-4 lg:px-14 px-4 ${isSticky ? "sticky top-0 right-0 left-0 border-b bg-white duration-300" : ""}`}>
+            <div className='flex justify-between items-center text-base gap-8'>
+                <a href="" className='text-2xl font-semibold flex items-center space-x-3'><img src={Logo} alt="" className='w-10 inline-block items-center' /><span className='text-[#263238]'>Nexcent</span></a>
 
             {/* nev items for large devices */}
             <ul className='md:flex space-x-12 hidden'>
@@ -51,6 +53,25 @@ const Navbar = () => {
             <div className='space-x-12 hidden lg:flex items-center'>
                 <a href="/" className='hidden lg:flex items-center text-brandPrimary hover:text-gray-900'>Login</a>
                 <button className='bg-brandPrimary text-white py-2 px-4 transition-all duration-300 rounded-lg hover:bg-neutralDGrey'>Sign Up</button>
+            </div>
+
+            {/* menu btn for olny mobile devices */}
+            <div className='md:hidden'>
+                <button
+                onClick={toggleMenu}
+                 className='text-neutralDGrey focus:outline-none focus:text-gray-500'>
+                    {
+                        isMenuOpen ? (<FaXmark className='h-6 w-6 '/>) : (<FaBars className='h-6 w-6 '/>)
+                    }
+                </button>
+            </div>
+            </div>
+
+            {/* nav items for mobile devices */}
+            <div className={`space-y-4 px-4 mt-16 py-7 bg-brandPrimary ${isMenuOpen ? "block fixed top-0 right-0 left-0 " : "hidden"}`}>
+                 {
+                    navItems.map(({link, path}) =><Link key={path} spy={true} smooth={true} offset={-100} to={path} className='block text-base hover:text-brandPrimary text-white first:font-medium'>{link}</Link>)
+                }
             </div>
         </nav>
     </header>
