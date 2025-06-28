bf41ccfff508c78c8e3c89eb2425709753542631
TanveenAmbrose <racoctanveen15@gmail.com>
2025-06-29 00:13:08 +0600

    dhoner carousel

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,33 @@
-import { Carousel } from 'flowbite-react'
-import React from 'react'
+import React, { useState } from 'react'
+import banner1 from '../assets/Illustration.svg'
 
 const Home = () => {
+
   return (
-    <div className='bg-neutralSilver'>
-      <div className='px-4 lg:px-14 max-w-screen-2xl mx-auto min-h-screen h-screen'>
-        <Carousel>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          Slide 1
-        </div>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          Slide 2
-        </div>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          Slide 3
-        </div>
-      </Carousel>
-      </div>
-    </div>
+  //  <div className='bg-neutralSilver font-now'>
+  //  <div className='px-4 lg-px-14 max-w-screen mx-auto min-h-screen h-screen'>
+
+      <div className="carousel w-full mx-auto bg-neutralSilver font-now">
+                <div id="slide1" className="carousel-item  w-full">
+                    <div className='my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse items-center justify-between gap-12'>
+                      <div>
+                        <img src={banner1} alt="" />
+                      </div>
+                      {/* hero text */}
+                      <div className='md:w-1/2 '>
+                        <h1 className='text-5xl font-semibold mb-4 text-neutralDGrey md:3/4 leading-snug'>Lessons and insights <span className='text-brandPrimary leading-sung'>from 8 years</span></h1>
+                        <p className='text-neutralDGrey text-base mb-8'>Where to grow your business as a photographer: site or social media?</p>
+                      </div>
+                    </div>
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                      <a href="#slide4" className="btn btn-circle ">❮</a>
+                      <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                  </div>
+  </div>
+
+  //    </div>
+  // </div> 
   )
 }
 
