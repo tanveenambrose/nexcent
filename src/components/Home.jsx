97ef29217598b5c4d803ff59c7cb0328d86025f5
TanveenAmbrose <racoctanveen15@gmail.com>
2025-06-29 13:51:26 +0600

    carousel in basic way using daisyui

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,30 +4,76 @@ import banner1 from '../assets/Illustration.svg'
 const Home = () => {
 
   return (
-  //  <div className='bg-neutralSilver font-now'>
-  //  <div className='px-4 lg-px-14 max-w-screen mx-auto min-h-screen h-screen'>
+   <div className='bg-neutralSilver font-now  h-screen'>
 
-      <div className="carousel w-full mx-auto bg-neutralSilver font-now">
-                <div id="slide1" className="carousel-item  w-full">
-                    <div className='my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse items-center justify-between gap-12'>
-                      <div>
-                        <img src={banner1} alt="" />
-                      </div>
-                      {/* hero text */}
-                      <div className='md:w-1/2 '>
-                        <h1 className='text-5xl font-semibold mb-4 text-neutralDGrey md:3/4 leading-snug'>Lessons and insights <span className='text-brandPrimary leading-sung'>from 8 years</span></h1>
-                        <p className='text-neutralDGrey text-base mb-8'>Where to grow your business as a photographer: site or social media?</p>
-                      </div>
-                    </div>
-                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                      <a href="#slide4" className="btn btn-circle ">❮</a>
-                      <a href="#slide2" className="btn btn-circle">❯</a>
-                    </div>
-                  </div>
+
+
+    <div className="carousel w-full bg-gray-300">
+
+
+  <div id="slide1" className="carousel-item relative flex w-full items-center justify-center pt-24">
+    <div className='flex items-center flex-col md:flex-row-reverse '>
+      <div className='flex-1 flex justify-center'><img className='' src={banner1} alt="" /></div>
+      <div className='flex-1 md:w-1/2 pb-10 pt-10'>
+        <h1 className='text-neutralDGrey text-5xl font-semibold mb-4 text-center leading-sung'>Lessons and insights <span className='text-brandPrimary leading-snug'>from 8 years</span></h1>
+        <p className='text-neutralGrey text-base mb-8 text-center'>Where to grow your business as a photographer: site or social media?</p>
+        <button className='btn-sexo ml-10'>Register</button>
+        </div>
+    </div>
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide3" className="btn btn-circle border-0 hover:border-2 bg-transparent text-black">❮</a>
+      <a href="#slide2" className="btn btn-circle border-0 hover:border-2 bg-transparent text-black">❯</a>
+    </div>
   </div>
 
-  //    </div>
-  // </div> 
+
+    <div id="slide2" className="carousel-item relative w-full flex items-center justify-center pt-24">
+    <div className='flex items-center flex-col md:flex-row-reverse '>
+      <div className='flex-1 flex justify-center'><img className='' src={banner1} alt="" /></div>
+      <div className='flex-1 md:w-1/2 pb-10 pt-10'>
+        <h1 className='text-neutralDGrey text-5xl font-semibold mb-4 text-center leading-sung'> Learn and Earn Money <span className='text-brandPrimary leading-snug'>in 4 Months</span></h1>
+        <p className='text-neutralGrey text-base mb-8 text-center'>Where to grow your business as a photographer: site or social media?</p>
+        <button className='btn-sexo ml-10'>Register</button>
+        </div>
+    </div>
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide1" className="btn btn-circle border-0 hover:border-2 bg-transparent text-black">❮</a>
+      <a href="#slide3" className="btn btn-circle border-0 hover:border-2 bg-transparent text-black">❯</a>
+    </div>
+  </div>
+
+
+
+
+
+  <div id="slide3" className="carousel-item relative w-full flex items-center justify-center pt-24">
+    <div className='flex items-center flex-col md:flex-row-reverse '>
+      <div className='flex-1 flex justify-center'><img className='' src={banner1} alt="" /></div>
+      <div className='flex-1 md:w-1/2 pb-10 pt-10'>
+        <h1 className='text-neutralDGrey text-5xl font-semibold mb-4 text-center leading-sung'>Lessons and insights <span className='text-brandPrimary leading-snug'>from 8 years</span></h1>
+        <p className='text-neutralGrey text-base mb-8 text-center'>Where to grow your business as a photographer: site or social media?</p>
+        <button className='btn-sexo ml-10'>Register</button>
+        </div>
+    </div>
+    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide2" className="btn btn-circle border-0 hover:border-2 bg-transparent text-black">❮</a>
+      <a href="#slide1" className="btn btn-circle border-0 hover:border-2 bg-transparent text-black">❯</a>
+    </div>
+  </div>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+   </div>
   )
 }
 
