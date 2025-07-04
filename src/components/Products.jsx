d923f7e5a4001ac674113b078a6c5c48722853e1
TanveenAmbrose <racoctanveen15@gmail.com>
2025-07-04 14:55:05 +0600

    i will finish it today

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import mobileLogo from '../assets/icons/mobile-logo.svg';
+import { GoArrowRight } from "react-icons/go";
 
 const Products = () => {
   return (
@@ -10,6 +11,36 @@ const Products = () => {
                   <h1 className='text-4xl text-neutralDGrey font-semibold mb-4 md:w-4/5'>How to design your site footer like we did</h1>
                   <p className='md:w-3/4 text-sm text-neutralGrey mb-8'>Donec a eros justo. Fusce egestas tristique ultrices. Nam tempor, augue nec tincidunt molestie, massa nunc varius arcu, at scelerisque elit erat a magna. Donec quis erat at libero ultrices mollis. In hac habitasse platea dictumst. Vivamus vehicula leo dui, at porta nisi facilisis finibus. In euismod augue vitae nisi ultricies, non aliquet urna tincidunt. Integer in nisi eget nulla commodo faucibus efficitur quis massa. Praesent felis est, finibus et nisi ac, hendrerit venenatis libero. Donec consectetur faucibus ipsum id gravida.</p>
                   <button className='btn-sexo'>Learn More</button>
+              </div>
+            </div>
+
+
+            {/* company info */}
+            <div className='px-4 lg:px-14 max-w-screen-2xl mx-auto bg-neutralSilver py-16'>
+              <div className='flex flex-col md:flex-row justify-between items-center gap-8'>
+                <div className='md:w-1/3'></div>
+                <div className='md:w-2/3 mx-auto'>
+                <p className='md:w-4/5 text-sm text-neutralGrey mb-8 leading-7'>Maecenas dignissim justo eget nulla rutrum molestie. Maecenas lobortis sem dui, vel rutrum risus tincidunt ullamcorper. Proin eu enim metus. Vivamus sed libero ornare, tristique quam in, gravida enim. Nullam ut molestie arcu, at hendrerit elit. Morbi laoreet elit at ligula molestie, nec molestie mi blandit. Suspendisse cursus tellus sed augue ultrices, quis tristique nulla sodales. Suspendisse eget lorem eu turpis vestibulum pretium. Suspendisse potenti. Quisque malesuada enim sapien, vitae placerat ante feugiat eget. Quisque vulputate odio neque, eget efficitur libero condimentum id. Curabitur id nibh id sem dignissim finibus ac sit amet magna.
+                </p>
+                <h5 className='text-brandPrimary text-xl font-semibold mb-2'>Tanveen Ambrose</h5>
+                <p className='text-sm text-gray-400'>British Dragon Boat Racing Association</p>
+
+                <div>
+                  <div className='flex flex-wrap justify-between items-center'>
+                    <img src="/src/assets/icons/company1.svg" alt="" />
+                    <img src="/src/assets/icons/company2.svg" alt="" />
+                    <img src="/src/assets/icons/company3.svg" alt="" />
+                    <img src="/src/assets/icons/company4.svg" alt="" />
+                    <img src="/src/assets/icons/company5.svg" alt="" />
+                    <img src="/src/assets/icons/company6.svg" alt="" />
+                    <img src="/src/assets/icons/company7.svg" alt="" />
+                    <div className=''>
+                      <a href="" className='font-bold flex text-brandPrimary hover:text-neutral-600'>Meet all customers<GoArrowRight className='w-8 mt-1'/></a>
+                    </div>
+                  </div>
+                </div>
+
+                </div>
               </div>
             </div>
     </div>
