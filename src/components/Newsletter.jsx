1f3cc1cc38751edf9ab0dcfa9e6b217ec674eb73
TanveenAmbrose <racoctanveen15@gmail.com>
2025-07-05 18:12:29 +0600

    doing these shits

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Newsletter = () => {
+  return (
+    <div className='px-4 lg:px-14 max-w-screen-2xl mx-auto bg-neutralSilver py-16 font-now'>
+        <div>
+            <div className='text-center '>
+                <h2 className='lg:text-4xl text-3xl text-neutral-800 font-semibold lg:leading-snug'>Pellentesque suscipit fringilla libero eu.</h2>
+                <div>
+                    <button className='btn-sexo'>Get a Demo</button>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Newsletter
