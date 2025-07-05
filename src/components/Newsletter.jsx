2ec8f6da0616f1a75c948f8ae47ae14e42d2d46b
TanveenAmbrose <racoctanveen15@gmail.com>
2025-07-05 18:26:01 +0600

    doing it

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import { GoPaperclip } from "react-icons/go";
 
 const Newsletter = () => {
   return (
     <div className='px-4 lg:px-14 max-w-screen-2xl mx-auto bg-neutralSilver py-16 font-now'>
         <div>
             <div className='text-center '>
-                <h2 className='lg:text-4xl text-3xl text-neutral-800 font-semibold lg:leading-snug'>Pellentesque suscipit fringilla libero eu.</h2>
-                <div>
-                    <button className='btn-sexo'>Get a Demo</button>
+                <h2 className='lg:text-4xl md:w-2/5 mx-auto text-3xl text-neutral-800 font-semibold lg:leading-snug'>Pellentesque suscipit fringilla libero eu.</h2>
+                <div className='flex justify-center items-center gap-8 pt-6'>
+                    <button className='btn-sexo flex mx-auto justify-center items-center gap-2'>Get a Demo <GoPaperclip/></button>
                 </div>
             </div>
         </div>
