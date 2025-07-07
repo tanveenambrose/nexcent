29c8246c10cbdfd5247081a5a4da7560070d2ccb
TanveenAmbrose <racoctanveen15@gmail.com>
2025-07-07 15:15:06 +0600

    ok

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
       transition={{duration: 2}}
       whileInView={{opacity: 1, x:0 }}
       viewport={{once: true}}
-     className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' id='faq'>
+     className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden bg-black' id='faq'>
       <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Contact <span className='underline underline-offset-4 decoration-1 under font-light'>With Us</span></h1>
       <p className='text-center text-gray-500 mb-12 max-w-80 mx-auto'>Ready to Make a Move? Let's Build Your Future Together</p>
 
